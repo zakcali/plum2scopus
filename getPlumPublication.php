7b850a4b05f77e6a5bbc5734a344795e1acf3e7b
Zafer Akçalı <zaferakcali@yahoo.com>
2022-12-28 12:23:01 +0300

    oop class library

---
+++ getPlumPublication.php
@@ -0,0 +1,111 @@
+<?php
+class getPlumPublication {
+	public $scopusid='', $doi='', $ArticleTitle='', $dergi='', $publisher='', $ISSN='', $eISSN='', $Year='', $Volume='', $Issue='', $StartPage='', $EndPage='', $yazarlar='', $PublicationType='', $AbstractText='', $PMID='', $atif='', $ISBN='';
+	public $yazarS=0;
+		    function __construct() {
+
+		}
+	final function plumPublication ($sid) {
+	
+	if($sid!=""){
+		if( substr($sid,0,7) != '2-s2.0-')
+	$sid='2-s2.0-'.$sid; // correct format
+	$preText="https://api.plu.mx/widget/elsevier/artifact?type=elsevier_id&id=";
+	$postText='';
+	$url = $preText.$sid;
+	$data=file_get_contents($url);
+// print_r ($data);
+	$plumBilgi=(json_decode($data, true));
+// var_dump ($plumBilgi);
+// print_r ($plumBilgi);
+	if (!isset ($plumBilgi['error_code'])) {// Not found
+		$plumId=str_replace('https://plu.mx/a/','',$plumBilgi['link']);
+		$plumLink='https://plu.mx/api/v1/artifact/id/'.$plumId;
+		$data2=file_get_contents($plumLink);
+		$scopusBilgi=(json_decode($data2, true));
+// print_r($scopusBilgi);
+// Makalenin başlığı
+		$this->ArticleTitle=$scopusBilgi['bibliographic_data']['artifact_title'];
+// yayın türü, çok güvenmemek gerek, vaka takdimleri de makale olabiliyor
+		if (isset($scopusBilgi['artifact_type']))
+			$this->PublicationType=$scopusBilgi['artifact_type'];
+// Özet
+		if (isset($scopusBilgi['bibliographic_data']['description']))
+			$this->AbstractText=$scopusBilgi['bibliographic_data']['description'];
+// doi
+		if (isset($scopusBilgi['identifier']['doi']))
+			$this->doi= $scopusBilgi['identifier']['doi'][0]['value'];
+// PMID
+		if (isset($scopusBilgi['identifier']['pmid']))
+			$this->PMID= $scopusBilgi['identifier']['pmid'][0]['value'];
+// scopus numarası = eid
+		foreach ($scopusBilgi['identifier']['url_id'] as $eleman) {
+			if (strpos ($eleman,'www.scopus.com') !== false) {
+			$start=strpos ($eleman,'&scp=')+5;
+			$end=strpos ($eleman,'&',$offset=$start);
+			$length=$end-$start;
+			$this->scopusid='2-s2.0-'.substr($eleman, $start,$length);
+			}
+		}
+// Dergi ismi
+		$this->dergi=$scopusBilgi['bibliographic_data']['publication_title'];
+// Aldığı atıf sayısı
+		if (isset ($scopusBilgi['plum_print_counts']['citation']))
+			$this->atif=$scopusBilgi['plum_print_counts']['citation']['total'];
+// yayınevi
+		if (isset($scopusBilgi['bibliographic_data']['publisher']))
+			$this->publisher = $scopusBilgi['bibliographic_data']['publisher'];
+// issn. eissn şimdiye kadar görülmedi
+		if (isset($scopusBilgi['bibliographic_data']['issn'])) {
+			$issntext=$scopusBilgi['bibliographic_data']['issn'];
+			$this->ISSN=substr ($issntext,0,4).'-'.substr ($issntext,4,4);
+		}
+// isbn, kitaplar için
+		if (isset($scopusBilgi['identifier']['isbn'][0]))
+			$this->ISBN=$scopusBilgi['identifier']['isbn'][0];
+		if (isset($scopusBilgi['identifier']['isbn'][1]))
+			$this->ISBN=$this->ISBN.'; '.$scopusBilgi['identifier']['isbn'][1];
+// Derginin basıldığı / yayımlandığı yıl
+		if (isset($scopusBilgi['bibliographic_data']['publication_year']))
+			$this->Year= $scopusBilgi['bibliographic_data']['publication_year'];
+// cilt
+		if (isset($scopusBilgi['bibliographic_data']['volume']))
+			$this->Volume=$scopusBilgi['bibliographic_data']['volume'];
+// sayı
+		if (isset($scopusBilgi['bibliographic_data']['issue']))
+			$this->Issue= $scopusBilgi['bibliographic_data']['issue'];
+
+// başlangıç-bitiş sayfası
+		if (isset($scopusBilgi['bibliographic_data']['start_page']))
+			$this->StartPage=$scopusBilgi['bibliographic_data']['start_page'];
+		if (isset($scopusBilgi['bibliographic_data']['end_page']))
+			$this->EndPage=$scopusBilgi['bibliographic_data']['end_page'];
+// başlangıç-bitiş sayfası
+		if (isset ($scopusBilgi['bibliographic_data']['page_range'])) { // sayfa numaraları
+			$sayfalar=explode ("-", $scopusBilgi['bibliographic_data']['page_range']);
+			$this->StartPage= $sayfalar[0];
+			if (isset ($sayfalar[1]))
+				$this->EndPage=$sayfalar[1];
+	}
+// yazarlar
+		$this->yazarlar="";
+// yazar sayısı
+		$this->yazarS=0;
+		if (isset ($scopusBilgi['bibliographic_data']['authors'] )) { // bu yayının yazarı yok: 85129231682
+			foreach( $scopusBilgi['bibliographic_data']['authors'] as $eleman) {
+				$isim='';
+				$soyisim='';
+				$soyadAd=explode (", ", $eleman);
+				$soyisim=$soyadAd[0];
+				if (isset($soyadAd[1]))
+					$isim=$soyadAd[1];
+				$this->yazarlar=$this->yazarlar.$isim." ".$soyisim.", ";
+				$this->yazarS=$this->yazarS+1;	
+				}
+		}
+		$this->yazarlar=substr ($this->yazarlar,0,-2);
+			} // NotFound hatası gelmedi
+		
+		} 
+	} // final function plumPublication 		
+}
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!-- plum2scopus V1.0: bu yazılım Dr. Zafer Akçalı tarafından oluşturulmuştur 
+<!-- plum2scopus V2.1: bu yazılım Dr. Zafer Akçalı tarafından oluşturulmuştur 
 programmed by Zafer Akçalı, MD -->
 <html>
 <head>
@@ -12,143 +12,49 @@ programmed by Zafer Akçalı, MD -->
 // plum2scopus
 // By Zafer Akçalı, MD
 // Zafer Akçalı tarafından programlanmıştır
-$scopusid=$doi=$ArticleTitle=$dergi=$publisher=$ISSN=$eISSN=$Year=$Volume=$Issue=$StartPage=$EndPage=$yazarlar=$PublicationType=$AbstractText=$PMID=$atif=$ISBN="";
-$yazarS=0;
+require_once './getPlumPublication.php';
+$s=new getPlumPublication ();
+
 if (isset($_POST['scopusid'])) {
 $gelenScopus=trim($_POST["scopusid"]);
 
 if($gelenScopus!=""){
-if( substr($gelenScopus,0,7) != '2-s2.0-')
-	$gelenScopus='2-s2.0-'.$gelenScopus; // correct format
-$preText="https://api.plu.mx/widget/elsevier/artifact?type=elsevier_id&id=";
-$postText='';
-$url = $preText.$gelenScopus;
-$data=file_get_contents($url);
-// print_r ($data);
-$plumBilgi=(json_decode($data, true));
-// var_dump ($plumBilgi);
-// print_r ($plumBilgi);
-if ( !isset ($plumBilgi['error_code'])) {// Not found
-$plumId=str_replace('https://plu.mx/a/','',$plumBilgi['link']);
-$plumLink='https://plu.mx/api/v1/artifact/id/'.$plumId;
-$data2=file_get_contents($plumLink);
-$scopusBilgi=(json_decode($data2, true));
-// print_r($scopusBilgi);
-// Makalenin başlığı
-$ArticleTitle=$scopusBilgi['bibliographic_data']['artifact_title'];
-// yayın türü, çok güvenmemek gerek, vaka takdimleri de makale olabiliyor
-if (isset($scopusBilgi['artifact_type']))
-	$PublicationType=$scopusBilgi['artifact_type'];
-// Özet
-if (isset($scopusBilgi['bibliographic_data']['description']))
-	$AbstractText=$scopusBilgi['bibliographic_data']['description'];
-// doi
-if (isset($scopusBilgi['identifier']['doi']))
-	$doi= $scopusBilgi['identifier']['doi'][0]['value'];
-// PMID
-if (isset($scopusBilgi['identifier']['pmid']))
-	$PMID= $scopusBilgi['identifier']['pmid'][0]['value'];
-// scopus numarası = eid
-foreach ($scopusBilgi['identifier']['url_id'] as $eleman) {
-	if (strpos ($eleman,'www.scopus.com') !== false) {
-		$start=strpos ($eleman,'&scp=')+5;
-		$end=strpos ($eleman,'&',$offset=$start);
-		$length=$end-$start;
-		$scopusid='2-s2.0-'.substr($eleman, $start,$length);
-	}
-}
-// Dergi ismi
-$dergi=$scopusBilgi['bibliographic_data']['publication_title'];
-// Aldığı atıf sayısı
-if (isset ($scopusBilgi['plum_print_counts']['citation']))
-	$atif=$scopusBilgi['plum_print_counts']['citation']['total'];
-// yayınevi
-if (isset($scopusBilgi['bibliographic_data']['publisher']))
-	$publisher = $scopusBilgi['bibliographic_data']['publisher'];
-
-// issn ve eissn
-if (isset($scopusBilgi['bibliographic_data']['issn'])) {
-	$issntext=$scopusBilgi['bibliographic_data']['issn'];
-	$ISSN=substr ($issntext,0,4).'-'.substr ($issntext,4,4);
-}
-// isbn, kitaplar için
-if (isset($scopusBilgi['identifier']['isbn'][0]))
-		$ISBN=$scopusBilgi['identifier']['isbn'][0];
-if (isset($scopusBilgi['identifier']['isbn'][1]))
-		$ISBN=$ISBN.'; '.$scopusBilgi['identifier']['isbn'][1];
-// Derginin basıldığı / yayımlandığı yıl
-if (isset($scopusBilgi['bibliographic_data']['publication_year']))
-	$Year= $scopusBilgi['bibliographic_data']['publication_year'];
-// cilt
-if (isset($scopusBilgi['bibliographic_data']['volume']))
-	$Volume= $scopusBilgi['bibliographic_data']['volume'];
-// sayı
-if (isset($scopusBilgi['bibliographic_data']['issue']))
-	$Issue= $scopusBilgi['bibliographic_data']['issue'];
-
-// başlangıç-bitiş sayfası
-if (isset($scopusBilgi['bibliographic_data']['start_page']))
-	$StartPage=$scopusBilgi['bibliographic_data']['start_page'];
-if (isset($scopusBilgi['bibliographic_data']['end_page']))
-	$EndPage=$scopusBilgi['bibliographic_data']['end_page'];
-// başlangıç-bitiş sayfası
-	if (isset ($scopusBilgi['bibliographic_data']['page_range'])) { // sayfa numaraları
-		$sayfalar=explode ("-", $scopusBilgi['bibliographic_data']['page_range']);
-		$StartPage= $sayfalar[0];
-		if (isset ($sayfalar[1]))
-			$EndPage=$sayfalar[1];
-	}
-// yazarlar
-$yazarlar="";
-// yazar sayısı
-$yazarS=0;
-foreach( $scopusBilgi['bibliographic_data']['authors'] as $eleman) {
-		$isim='';
-		$soyisim='';
-		$soyadAd=explode (", ", $eleman);
-		$soyisim=$soyadAd[0];
-			if (isset($soyadAd[1]))
-				$isim=$soyadAd[1];
-		$yazarlar=$yazarlar.$isim." ".$soyisim.", ";
-		$yazarS=$yazarS+1;	
-		}
-$yazarlar=substr ($yazarlar,0,-2);
-			} // NotFound hatası gelmedi
-		
+	$s->plumPublication ($gelenScopus);
 	} 
 }
 ?>
 <a href="eid nerede.png" target="_blank"> Scopus numarasına nereden bakılır? </a>
 <form method="post" action="">
 Scopus makale numarasını (eid) giriniz<br/>
-<input type="text" name="scopusid" id="scopusid" value="<?php echo $scopusid;?>" >
+<input type="text" name="scopusid" id="scopusid" value="<?php echo $s->scopusid;?>" >
 <input type="submit" value="Scopus yayın bilgilerini PHP ile getir">
 </form>
 <button id="scopusGoster" onclick="scopusGoster()">Scopus yayınını göster</button>
 <button id="scopusAtifGoster" onclick="scopusAtifGoster()">Scopus yayınının atıflarını göster</button>
+<button id="pubmedGit" onclick="pubmedGit()">pubmed ile yayına git</button>
 <button id="doiGit" onclick="doiGit()">doi ile makaleyi göster</button>
 <br/>
-Scopus eid: <input type="text" name="eid" size="25" id="eid" value="<?php echo $scopusid;?>" >  
-doi: <input type="text" name="doi" size="55"  id="doi" value="<?php echo $doi;?>"> <br/>
-Makalenin başlığı: <input type="text" name="ArticleTitle" size="85"  id="ArticleTitle" value="<?php echo $ArticleTitle;?>"> <br/>
-Dergi ismi: <input type="text" name="Title" size="50"  id="Title" value="<?php echo $dergi;?>"> 
-Yayınevi: <input type="text" name="publisher" size="26"  id="publisher" value="<?php echo $publisher;?>"> <br/>
-ISSN: <input type="text" name="ISSN" size="8"  id="ISSN" value="<?php echo $ISSN;?>">
-eISSN: <input type="text" name="eISSN" size="8"  id="eISSN" value="<?php echo $eISSN;?>">
-ISBN: <input type="text" name="ISBN" size="30"  id="ISBN" value="<?php echo $ISBN;?>"> <br/>
-Yıl: <input type="text" name="Year" size="4"  id="Year" value="<?php echo $Year;?>">
-Cilt: <input type="text" name="Volume" size="2"  id="Volume" value="<?php echo $Volume;?>">
-Sayı: <input type="text" name="Issue" size="2"  id="Issue" value="<?php echo $Issue;?>">
-Sayfa/numara: <input type="text" name="StartPage" size="5"  id="StartPage" value="<?php echo $StartPage;?>">
-- <input type="text" name="EndPage" size="2"  id="EndPage" value="<?php echo $EndPage;?>">
-Yazar sayısı: <input type="text" name="yazarS" size="2"  id="yazarS" value="<?php echo $yazarS;?>"><br/>
-Yazarlar: <input type="text" name="yazarlar" size="95"  id="yazarlar" value="<?php echo $yazarlar;?>"><br/>
-Yayın türü: <input type="text" name="PublicationType" size="20"  id="PublicationType" value="<?php echo $PublicationType;?>">
-PMID: <input type="text" name="PMID" size="6"  id="PMID" value="<?php echo $PMID;?>">
-Aldığı atıf: <input type="text" name="citedBy" size="4"  id="citedBy" value="<?php echo $atif;?>">
+Scopus eid: <input type="text" name="eid" size="25" id="eid" value="<?php echo $s->scopusid;?>" >  
+doi: <input type="text" name="doi" size="55"  id="doi" value="<?php echo $s->doi;?>"> <br/>
+Makalenin başlığı: <input type="text" name="ArticleTitle" size="85"  id="ArticleTitle" value="<?php echo str_replace ('"',  '&#34',$s->ArticleTitle);?>"> <br>
+Dergi ismi: <input type="text" name="Title" size="50"  id="Title" value="<?php echo $s->dergi;?>"> 
+Yayınevi: <input type="text" name="publisher" size="26"  id="publisher" value="<?php echo $s->publisher;?>"> <br/>
+ISSN: <input type="text" name="ISSN" size="8"  id="ISSN" value="<?php echo $s->ISSN;?>">
+eISSN: <input type="text" name="eISSN" size="8"  id="eISSN" value="<?php echo $s->eISSN;?>">
+ISBN: <input type="text" name="ISBN" size="30"  id="ISBN" value="<?php echo $s->ISBN;?>"> <br/>
+Yıl: <input type="text" name="Year" size="4"  id="Year" value="<?php echo $s->Year;?>">
+Cilt: <input type="text" name="Volume" size="2"  id="Volume" value="<?php echo $s->Volume;?>">
+Sayı: <input type="text" name="Issue" size="2"  id="Issue" value="<?php echo $s->Issue;?>">
+Sayfa/numara: <input type="text" name="StartPage" size="5"  id="StartPage" value="<?php echo $s->StartPage;?>">
+- <input type="text" name="EndPage" size="2"  id="EndPage" value="<?php echo $s->EndPage;?>">
+Yazar sayısı: <input type="text" name="yazarS" size="2"  id="yazarS" value="<?php echo $s->yazarS;?>"><br/>
+Yazarlar: <input type="text" name="yazarlar" size="95"  id="yazarlar" value="<?php echo $s->yazarlar;?>"><br/>
+Yayın türü: <input type="text" name="PublicationType" size="20"  id="PublicationType" value="<?php echo $s->PublicationType;?>">
+PMID: <input type="text" name="pmid" size="6"  id="pmid" value="<?php echo $s->PMID;?>">
+Aldığı atıf: <input type="text" name="citedBy" size="4"  id="citedBy" value="<?php echo $s->atif;?>">
 <br/>
 Özet <br/>
-<textarea rows = "20" cols = "90" name = "ozet" id="ozetAlan"><?php echo $AbstractText;?></textarea>  <br/>
+<textarea rows = "20" cols = "90" name = "ozet" id="ozetAlan"><?php echo $s->AbstractText;?></textarea>  <br/>
 <script>
 function scopusGoster() {
 var	w=document.getElementById('eid').value.replace('2-s2.0-','');
@@ -162,7 +68,14 @@ var	w=document.getElementById('eid').value;
 }
 function doiGit() {
 var	w=document.getElementById('doi').value;
-	urlText = "https://doi.org/"+w;
+urlText = "https://doi.org/"+w;
+if ( w != '')
+	window.open(urlText,"_blank");
+}
+function pubmedGit() {
+var	w=document.getElementById('pmid').value.replace(/\D/g, "");
+urlText = "https://pubmed.ncbi.nlm.nih.gov/"+w;
+if ( w != '')
 	window.open(urlText,"_blank");
 }
 </script>
