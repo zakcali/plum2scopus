e6e6ff546349bc6c2e9f254ef502479c49ce1ebb
Zafer Akçalı <zaferakcali@yahoo.com>
2022-12-28 15:22:17 +0300

    oop class library

--- index.php
+++ index.php
@@ -37,24 +37,24 @@ Scopus makale numarasını (eid) giriniz<br/>
 Scopus eid: <input type="text" name="eid" size="25" id="eid" value="<?php echo $s->scopusid;?>" >  
 doi: <input type="text" name="doi" size="55"  id="doi" value="<?php echo $s->doi;?>"> <br/>
 Makalenin başlığı: <input type="text" name="ArticleTitle" size="85"  id="ArticleTitle" value="<?php echo str_replace ('"',  '&#34',$s->ArticleTitle);?>"> <br>
-Dergi ismi: <input type="text" name="Title" size="50"  id="Title" value="<?php echo $s->dergi;?>"> 
-Yayınevi: <input type="text" name="publisher" size="26"  id="publisher" value="<?php echo $s->publisher;?>"> <br/>
+Dergi ismi: <input type="text" name="Title" size="80"  id="Title" value="<?php echo $s->dergi;?>"><br> 
 ISSN: <input type="text" name="ISSN" size="8"  id="ISSN" value="<?php echo $s->ISSN;?>">
 eISSN: <input type="text" name="eISSN" size="8"  id="eISSN" value="<?php echo $s->eISSN;?>">
-ISBN: <input type="text" name="ISBN" size="30"  id="ISBN" value="<?php echo $s->ISBN;?>"> <br/>
+ISBN: <input type="text" name="ISBN" size="30"  id="ISBN" value="<?php echo $s->ISBN;?>"> <br>
 Yıl: <input type="text" name="Year" size="4"  id="Year" value="<?php echo $s->Year;?>">
 Cilt: <input type="text" name="Volume" size="2"  id="Volume" value="<?php echo $s->Volume;?>">
 Sayı: <input type="text" name="Issue" size="2"  id="Issue" value="<?php echo $s->Issue;?>">
 Sayfa/numara: <input type="text" name="StartPage" size="5"  id="StartPage" value="<?php echo $s->StartPage;?>">
 - <input type="text" name="EndPage" size="2"  id="EndPage" value="<?php echo $s->EndPage;?>">
-Yazar sayısı: <input type="text" name="yazarS" size="2"  id="yazarS" value="<?php echo $s->yazarS;?>"><br/>
-Yazarlar: <input type="text" name="yazarlar" size="95"  id="yazarlar" value="<?php echo $s->yazarlar;?>"><br/>
+Yazar sayısı: <input type="text" name="yazarS" size="2"  id="yazarS" value="<?php echo $s->yazarS;?>"><br>
+Yazarlar: <input type="text" name="yazarlar" size="95"  id="yazarlar" value="<?php echo $s->yazarlar;?>"><br>
+Yayınevi: <input type="text" name="publisher" size="26"  id="publisher" value="<?php echo $s->publisher;?>"> <br>
 Yayın türü: <input type="text" name="PublicationType" size="20"  id="PublicationType" value="<?php echo $s->PublicationType;?>">
 PMID: <input type="text" name="pmid" size="6"  id="pmid" value="<?php echo $s->PMID;?>">
 Aldığı atıf: <input type="text" name="citedBy" size="4"  id="citedBy" value="<?php echo $s->atif;?>">
 <br/>
 Özet <br/>
-<textarea rows = "20" cols = "90" name = "ozet" id="ozetAlan"><?php echo $s->AbstractText;?></textarea>  <br/>
+<textarea rows = "20" cols = "100" name = "ozet" id="ozetAlan"><?php echo $s->AbstractText;?></textarea>  <br/>
 <script>
 function scopusGoster() {
 var	w=document.getElementById('eid').value.replace('2-s2.0-','');
